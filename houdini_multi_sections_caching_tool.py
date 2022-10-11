65c3cb61f447ecc97774fcfec7504654fb217216
炮灰舒芙琳 <89049506+SeasomeMaggot@users.noreply.github.com>
2022-10-11 16:47:10 +0800

    Update houdini_multi_sections_caching_tool.py

--- houdini_multi_sections_caching_tool.py
+++ houdini_multi_sections_caching_tool.py
@@ -27,8 +27,16 @@ class Stats():
 
     def handleCalc(self):
     
-        sections = int(self.lineEdit.text())
-        
+        try:
+            sections = int(self.lineEdit.text())
+        except:
+            QMessageBox.about(self.window,
+                    'Error',
+                    'Set Section Number!!!'
+                    )
+            raise           
+#------------check frame range----------------
+                 
         selNode = hou.selectedNodes()[0]
         selType = selNode.type()
         parent = selNode.parent()
@@ -45,50 +53,50 @@ class Stats():
                     'Set Frame Range!!!'
                     )
 #------------check frame range----------------
-        elif sections is None:
-             QMessageBox.about(self.window,
-                    'Error',
-                    'Set Section Number!!!'
-                    )
                 
         else:
-            sf = selNode.parm('f1').eval()
-            ef = selNode.parm('f2').eval()
-            fr = ef - sf + 1
-            sr = fr/sections
-            
-            path = selNode.parm('file').unexpandedString()
-            
-            subNode = parent.createNode('subnet','TMP_multi_caching_tool_subnet')
-            subNode.setColor(hou.Color(0,0,0))            
-            input = subNode.path()+'/1'
-            subNode.moveToGoodPosition()
-            
-            ropList = []
-            
-            for n in range(sections):
-            
-                copyNode = subNode.createNode('rop_geometry')
-                copyNode.setName('TMP_multi_caching_tool_section'+str(n))
-                copyNode.parm('trange').set(1)
-                copyNode.parmTuple('f').deleteAllKeyframes()
-                copyNode.moveToGoodPosition()
-                copyNode.setInput(0,hou.item(input))
+        
+            ret = QMessageBox.question(self.window,
+                    'Save!',
+                    'You must save file before caching!!!'
+                    )
+            if ret == QMessageBox.Yes:
+                    
+                sf = selNode.parm('f1').eval()
+                ef = selNode.parm('f2').eval()
+                fr = ef - sf + 1
+                sr = fr/sections
                 
-                ropList.append(copyNode)
-#-----------------create instances-------------------------------------
-    
-                if n == 0:
-                    copyNode.setParms({'f1':(sections-1)*sr+1,})
-                    copyNode.setName('TMP_multi_caching_tool_section'+str(sections))
+                path = selNode.parm('file').unexpandedString()
+                
+                subNode = parent.createNode('subnet','TMP_multi_caching_tool_subnet')
+                subNode.setColor(hou.Color(0,0,0))            
+                input = subNode.path()+'/1'
+                subNode.moveToGoodPosition()
+                
+                ropList = []
+                
+                for n in range(sections):
+                
+                    copyNode = subNode.createNode('rop_geometry')
+                    copyNode.setName('TMP_multi_caching_tool_section'+str(n))
+                    copyNode.parm('trange').set(1)
+                    copyNode.parmTuple('f').deleteAllKeyframes()
+                    copyNode.moveToGoodPosition()
+                    copyNode.setInput(0,hou.item(input))
                     
-                else:
-                    copyNode.setParms({'f1':sf+sr*(n-1),'f2':sf+sr*n-1})
+                    ropList.append(copyNode)
+    #-----------------create instances-------------------------------------
+        
+                    if n == 0:
+                        copyNode.setParms({'f1':(sections-1)*sr+1,})
+                        copyNode.setName('TMP_multi_caching_tool_section'+str(sections))
+                        
+                    else:
+                        copyNode.setParms({'f1':sf+sr*(n-1),'f2':sf+sr*n-1})
                     
             
-    
-            for i in ropList:
-                i.parm('executebackground').pressButton()
+            
         
 
 stats = Stats()
