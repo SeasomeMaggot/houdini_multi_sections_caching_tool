f2e8277a10ad550bf79a95a15ef088ad908d1d1d
炮灰舒芙琳 <89049506+SeasomeMaggot@users.noreply.github.com>
2022-10-11 11:57:30 +0800

    Update houdini_multi_sections_caching_tool.py

--- houdini_multi_sections_caching_tool.py
+++ houdini_multi_sections_caching_tool.py
@@ -11,7 +11,7 @@ class Stats():
         self.window.setWindowTitle('Multi Sections Caching Tool')
 
         self.lineEdit = QLineEdit(self.window)
-        self.lineEdit.setPlaceholderText("Input section number")
+        self.lineEdit.setPlaceholderText("Input section number (Warning: No $OS in file path!)")
         self.onlyInt = QIntValidator()
         self.onlyInt.setRange(0, 9999)
         self.lineEdit.setValidator(self.onlyInt)
@@ -49,20 +49,23 @@ class Stats():
             sf = selNode.parm('f1').eval()
             ef = selNode.parm('f2').eval()
             fr = ef - sf + 1
-            lf = fr % sections
-            
-        print(sf,ef,fr,lf)
+            sr = fr/sections
             
         for n in range(sections):       
-            copyNode = parent.copyItems((selNode,))
+            copyNode = parent.copyItems((selNode,))[0]
             copyNode.setName('TMP_multi_caching_tool_section'+str(n))
+            copyNode.parmTuple('f').deleteAllKeyframes()
             copyNode.moveToGoodPosition()
+            copyNode.parm('tpostrender').set(1)
+            copyNode.parm('lpostrender').set(1)
+            copyNode.parm('postrender').set()
             
-            if n == sections:
-                pass
+            if n == 0:
+                copyNode.setParms({'f1':(sections-1)*sr+1,})
+                copyNode.setName('TMP_multi_caching_tool_section'+str(sections))
                 
             else:
-                copyNode.setParms({'f1':sf+section*n,'f2':sf+section*(n+1)})
+                copyNode.setParms({'f1':sf+sr*(n-1),'f2':sf+sr*n-1})
 
         
         
